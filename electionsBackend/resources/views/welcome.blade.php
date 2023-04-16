c37778ddf65520856d61e8cc43ae1395a3aefeba
nguetcheu <dnguetcheu@gmail.com>
2023-04-16 19:29:14 +0200

    installation de vit pour le support  de bootsrap Tp1

--- electionsBackend/resources/views/welcome.blade.php
+++ electionsBackend/resources/views/welcome.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @vite(['resources/js/app.js'])
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+       
+
+        <!-- Styles -->
+       
     </head>
-    <body class="antialiased">
-        
+    <body>
+        <h1 class="alert alert-success"> bonjour csi3 </h1>
+        <a href="/regions_create"> create region </a>
     </body>
 </html>
