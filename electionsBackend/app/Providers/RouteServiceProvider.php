51757b9bec949dfdff6d1a0e9231e4402c93ad69
nguetcheu <dnguetcheu@gmail.com>
2023-04-17 15:52:37 +0200

    Ajout de la reconnaissance de  l'ensemble des controllers

--- electionsBackend/app/Providers/RouteServiceProvider.php
+++ electionsBackend/app/Providers/RouteServiceProvider.php
@@ -32,6 +32,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace('APP/Http/Controllers')
                 ->group(base_path('routes/web.php'));
         });
     }
