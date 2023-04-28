44ffba8f90f1bbe87d710b6a234ffffb5acc3f0b
nguetcheu <dnguetcheu@gmail.com>
2023-04-28 02:36:01 +0200

    Ajout de commentaire

--- electionsFrontEnd/src/app/services/region.service.ts
+++ electionsFrontEnd/src/app/services/region.service.ts
@@ -11,11 +11,12 @@ export class RegionService {
 
   constructor(public http: HttpClient) {}
 
+  // Création d'un observable qui va émettre une région qui sera souscris dans le comosant region 
   insertRegion(region: Region): Observable<Region> {
     return this.http.post<Region>(this.CREATE_REGION, region);
   }
 
-  // Création d'un observable qui va émettre un tableau de région qui sera
+  // Création d'un observable qui va émettre un tableau de région qui sera souscris dans le comosant region 
   loadRegion(): Observable<Region[]> {
     return this.http.get<Region[]>('http://localhost:8000/api/region');
   }
